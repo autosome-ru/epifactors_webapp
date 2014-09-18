a707a6cf826054f53ca4b5ded172323989ef5b43
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-09-19 01:34:47 +0400

    better svg transformation for rescaling

--- app/assets/javascripts/animate_svg.js
+++ app/assets/javascripts/animate_svg.js
@@ -49,6 +49,16 @@ restore_attr = function(selector, attr) {
   });
 };
 
+// sx  0   cx(1-sx)
+// 0   sy  cy(1-sy)
+// 0   0   1
+rescale = function(element, scale_x, scale_y) {
+  var coords = element.getBBox();
+  var cx = coords.x + coords.width / 2.0;
+  var cy = coords.y + coords.height / 2.0;
+  $(element).css('transform','matrix('+[scale_x,0, 0, scale_y, cx*(1-scale_x), cy*(1-scale_y)].join(',')+')')
+};
+
 animate_svg = function() {
   var image = $('#main_scheme_svg');
   var epigenes = image.find('g.epigene');
@@ -59,16 +69,18 @@ animate_svg = function() {
       var same_epigenes = epigenes.filter('[data-epigene-name="'+name+'"]');
 
       set_css_attr(same_epigenes.find('path'), 'fill', function(){return 'red';} );
-      set_css_attr(same_epigenes.find('text, tspan'), 'font-size', function(font_size){return multiplicate_size(font_size,1.1);} );
-      set_css_attr(same_epigenes.find('text, tspan'), 'font-weight', function(){return 'bolder';} );
+      // set_css_attr(same_epigenes.find('text, tspan'), 'font-size', function(font_size){return multiplicate_size(font_size,1.1);} );
+      // set_css_attr(same_epigenes.find('text, tspan'), 'font-weight', function(){return 'bolder';} );
+      same_epigenes.each(function(i,el){ rescale(el,1.5,1.5); });
     },
     function(e) {
       var name = $(e.target).closest('g.epigene').data('epigene-name');
       var same_epigenes = epigenes.filter('[data-epigene-name="'+name+'"]');
 
       restore_css_attr(same_epigenes.find('path'), 'fill');
-      restore_css_attr(same_epigenes.find('text, tspan'), 'font-size');
-      restore_css_attr(same_epigenes.find('text, tspan'), 'font-weight');
+      // restore_css_attr(same_epigenes.find('text, tspan'), 'font-size');
+      // restore_css_attr(same_epigenes.find('text, tspan'), 'font-weight');
+      same_epigenes.each(function(i,el){ rescale(el,1,1); });
     }
   );
 
@@ -88,22 +100,24 @@ animate_svg = function() {
       var same_histones = histones.filter('[data-histone-name="'+name+'"]');
 
       // set_css_attr(same_histones.find('path'), 'fill', function(){return 'red';} );
-      set_css_attr(same_histones.find('text, tspan'), 'font-size', function(font_size){return multiplicate_size(font_size,1.5);} );
-      set_css_attr(same_histones.find('text, tspan'), 'font-weight', function(){return 'bolder';} );
-      set_attr(same_histones.find('circle'), 'r', function(r){return r*2;} );
-      set_attr(same_histones.find('ellipse'), 'rx', function(rx){return rx*2;} );
-      set_attr(same_histones.find('ellipse'), 'ry', function(ry){return ry*2;} );
+      // set_css_attr(same_histones.find('text, tspan'), 'font-size', function(font_size){return multiplicate_size(font_size,1.5);} );
+      // set_css_attr(same_histones.find('text, tspan'), 'font-weight', function(){return 'bolder';} );
+      // // set_attr(same_histones.find('circle'), 'r', function(r){return r*2;} );
+      // set_attr(same_histones.find('ellipse'), 'rx', function(rx){return rx*2;} );
+      // set_attr(same_histones.find('ellipse'), 'ry', function(ry){return ry*2;} );
+      same_histones.each(function(i,el){ rescale(el,2.5,2.5); });
     },
     function(e) {
       var name = $(e.target).closest('g.histone').data('histone-name');
       var same_histones = histones.filter('[data-histone-name="'+name+'"]');
 
       // restore_css_attr(same_histones.find('path'), 'fill');
-      restore_css_attr(same_histones.find('text, tspan'), 'font-size');
-      restore_css_attr(same_histones.find('text, tspan'), 'font-weight');
-      restore_attr(same_histones.find('circle'), 'r');
-      restore_attr(same_histones.find('ellipse'), 'rx');
-      restore_attr(same_histones.find('ellipse'), 'ry');
+      // restore_css_attr(same_histones.find('text, tspan'), 'font-size');
+      // restore_css_attr(same_histones.find('text, tspan'), 'font-weight');
+      // restore_attr(same_histones.find('circle'), 'r');
+      // restore_attr(same_histones.find('ellipse'), 'rx');
+      // restore_attr(same_histones.find('ellipse'), 'ry');
+      same_histones.each(function(i,el){ rescale(el,1,1); });
     }
   );
 };
