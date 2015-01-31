f3ce6d038d0b3ec4a3598e1ffc83cbe96a0e7bb0
prijutme4ty <prijutme4ty@gmail.com>
2015-02-01 02:36:13 +0300

    don't show group index and don't search by it (it's just a number)

--- app/models/protein_complex.rb
+++ app/models/protein_complex.rb
@@ -14,9 +14,7 @@ class ProteinComplex < ActiveRecord::Base
   #   Gene.where(uniprot_id: uniprot_ids_splitted)
   # end
 
-  # searchable_attributes = [ :complex_group,:complex_group_name,:complex_name,:alternative_names,:proteins_involved,
-  #                           :uniprot_ids,:funct,:specific_target,:target_molecule,:product,:target_uniprot_ids,:details]
-  searchable_attributes = [ :group, :group_name, :complex_name, :status, :alternative_name, :protein, :uniprot_id,
+  searchable_attributes = [ :group_name, :complex_name, :status, :alternative_name, :protein, :uniprot_id,
                             :function, :target, :specific_target, :product, :uniprot_id_target, :comment ]
   include SearchableFullText
   searchable_by(searchable_attributes)
