111fdf29bd928326f44c898d8dfa661007cba87f
prijutme4ty <prijutme4ty@gmail.com>
2015-03-13 07:11:05 +0300

    updated svg

--- app/assets/javascripts/animate_svg.js
+++ app/assets/javascripts/animate_svg.js
@@ -56,12 +56,17 @@ rescale = function(element, scale_x, scale_y) {
   var coords = element.getBBox();
   var cx = coords.x + coords.width / 2.0;
   var cy = coords.y + coords.height / 2.0;
-  $(element).css('transform','matrix('+[scale_x,0, 0, scale_y, cx*(1-scale_x), cy*(1-scale_y)].join(',')+')')
+  $(element).css('transform','matrix('+[scale_x,0, 0, scale_y, cx*(1-scale_x), cy*(1-scale_y)].join(',')+')');
 };
+
+rescale_svg = function(element, scale_x, scale_y) {
+  $(element).children().attr('transform', 'scale('+[scale_x, scale_y].join(',')+')');
+}
 
 animate_svg = function() {
   var image = $('#main_scheme_svg');
   var epigenes = image.find('g.epigene');
+  var epigenes_1 = image.find('g.epigene_1');
   var histones = image.find('g.histone');
   epigenes.hover(
     function(e) {
@@ -69,8 +74,6 @@ animate_svg = function() {
       var same_epigenes = epigenes.filter('[data-epigene-name="'+name+'"]');
 
       set_css_attr(same_epigenes.find('path'), 'fill', function(){return 'red';} );
-      // set_css_attr(same_epigenes.find('text, tspan'), 'font-size', function(font_size){return multiplicate_size(font_size,1.1);} );
-      // set_css_attr(same_epigenes.find('text, tspan'), 'font-weight', function(){return 'bolder';} );
       same_epigenes.each(function(i,el){ rescale(el,1.5,1.5); });
     },
     function(e) {
@@ -78,14 +81,36 @@ animate_svg = function() {
       var same_epigenes = epigenes.filter('[data-epigene-name="'+name+'"]');
 
       restore_css_attr(same_epigenes.find('path'), 'fill');
-      // restore_css_attr(same_epigenes.find('text, tspan'), 'font-size');
-      // restore_css_attr(same_epigenes.find('text, tspan'), 'font-weight');
       same_epigenes.each(function(i,el){ rescale(el,1,1); });
+    }
+  );
+
+  epigenes_1.hover(
+    function(e) {
+      var name = $(e.target).closest('g.epigene_1').data('epigene-name');
+      var same_epigenes = epigenes_1.filter('[data-epigene-name="'+name+'"]');
+
+      $(e.target).closest('g.epigene_1')[0].classList.add('emphasized');
+      set_css_attr(same_epigenes.find('use'), 'fill', function(){return 'red';} );
+      same_epigenes.each(function(i,el){ rescale_svg(el,1.5,1.5); });
+    },
+    function(e) {
+      var name = $(e.target).closest('g.epigene_1').data('epigene-name');
+      var same_epigenes = epigenes_1.filter('[data-epigene-name="'+name+'"]');
+
+      $(e.target).closest('g.epigene_1')[0].classList.remove('emphasized');
+      restore_css_attr(same_epigenes.find('use'), 'fill');
+      same_epigenes.each(function(i,el){ rescale_svg(el,1,1); });
     }
   );
 
   epigenes.click(function(e) {
     var name = $(e.target).closest('g.epigene').data('epigene-name');
+    window.location = '/protein_complexes?search='+name+'&field=group_name&similar=1';
+  });
+
+  epigenes_1.click(function(e) {
+    var name = $(e.target).closest('g.epigene_1').data('epigene-name');
     window.location = '/protein_complexes?search='+name+'&field=group_name&similar=1';
   });
 
@@ -99,24 +124,12 @@ animate_svg = function() {
       var name = $(e.target).closest('g.histone').data('histone-name');
       var same_histones = histones.filter('[data-histone-name="'+name+'"]');
 
-      // set_css_attr(same_histones.find('path'), 'fill', function(){return 'red';} );
-      // set_css_attr(same_histones.find('text, tspan'), 'font-size', function(font_size){return multiplicate_size(font_size,1.5);} );
-      // set_css_attr(same_histones.find('text, tspan'), 'font-weight', function(){return 'bolder';} );
-      // // set_attr(same_histones.find('circle'), 'r', function(r){return r*2;} );
-      // set_attr(same_histones.find('ellipse'), 'rx', function(rx){return rx*2;} );
-      // set_attr(same_histones.find('ellipse'), 'ry', function(ry){return ry*2;} );
       same_histones.each(function(i,el){ rescale(el,2.5,2.5); });
     },
     function(e) {
       var name = $(e.target).closest('g.histone').data('histone-name');
       var same_histones = histones.filter('[data-histone-name="'+name+'"]');
 
-      // restore_css_attr(same_histones.find('path'), 'fill');
-      // restore_css_attr(same_histones.find('text, tspan'), 'font-size');
-      // restore_css_attr(same_histones.find('text, tspan'), 'font-weight');
-      // restore_attr(same_histones.find('circle'), 'r');
-      // restore_attr(same_histones.find('ellipse'), 'rx');
-      // restore_attr(same_histones.find('ellipse'), 'ry');
       same_histones.each(function(i,el){ rescale(el,1,1); });
     }
   );
