4429af7233128be289c91328819244745ec7ada7
prijutme4ty <prijutme4ty@gmail.com>
2015-05-22 15:38:59 +0300

    help --> tooltip

--- app/assets/javascripts/expression_table.js
+++ app/assets/javascripts/expression_table.js
@@ -50,12 +50,12 @@ var page_ready = function() {
     )
   );
 
-  $('.help-icon, .help-text').hover(
+  $('.help').hover(
     function(e){
       $(e.target).parent().find('.help-text').show();
     }
   );
-  $('.help-icon, .help-text').parent().hover(null,
+  $('.help').parent().hover(null,
     function(e){
       console.log(e, e.currentTarget);
       $(e.currentTarget).find('.help-text').hide();
