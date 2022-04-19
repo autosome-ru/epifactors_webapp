f834c3c28a942278d6c6b18705c1d4a1fecd2545
Danya-2003 <marakulina.da@phystech.edu>
2022-04-20 01:06:18 +0300

    update config, db, lib/tasks

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module EpigeneDB
-  DATA_VERSION = '1.7.3'
+  DATA_VERSION = '1.8.0'
 
   class Application < Rails::Application
     config.before_configuration do
