1ca26e246c3adbb119ba40122732603acbe84773
Danya-2003 <marakulina.da@phystech.edu>
2022-08-14 17:02:26 +0300

    v2.0

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module EpigeneDB
-  DATA_VERSION = '1.8.0'
+  DATA_VERSION = '2.0'
 
   class Application < Rails::Application
     config.before_configuration do
