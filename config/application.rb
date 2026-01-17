c868549292d69904f17f4ee6f4df2220f006fec1
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2026-01-18 02:04:04 +0300

    fix File.exists? → File.exist?

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Epifactors
   class Application < Rails::Application
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'env.yaml')
-      next  unless File.exists?(env_file)
+      next  unless File.exist?(env_file)
       YAML.load_file(env_file).each{|k,v| ENV[k] ||= v }
     end
 
